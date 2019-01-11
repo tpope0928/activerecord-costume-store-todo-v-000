7890b7b7705f8d070a5641ba1d05cfdfbdbc71de
Thomas Pope <tpope0928@gmail.com>
2019-01-11 15:16:41 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.boolean "family_friendly"
     t.datetime "opening_date"
     t.datetime "closing_date"
-    t.text "long_description"
+    t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
