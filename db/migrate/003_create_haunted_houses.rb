bb07459955138b4086e714dd6e8ab7c3b533c0c4
Thomas Pope <tpope0928@gmail.com>
2019-01-11 15:12:44 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,14 +2,14 @@
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
     create_table :haunted_houses do |t|
-      :name
-      :location
-      :theme
-      :price
-      :family_frindly
-      :opening_date
-      :closing_date
-      :long_description
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_frindly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :long_description
 
       t.timestamps
     end
